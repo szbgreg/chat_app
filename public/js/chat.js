6c11054f0d75e070b100d12dfa549f21e47bd42b
szbgreg <szbgreg@gmail.com>
2020-12-12 11:08:31 +0100

    Sharing location with others

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,35 @@
 const socket = io();
 
+// Show message if we get 'message' event from server!
 socket.on('message', (data) => console.log(data));
 
+// We send a 'sendMessage' event to server with the
+// form input value on submit btn click
 document.querySelector('#message-form').addEventListener('submit', (e) => {
   e.preventDefault();
 
   const message = e.target.elements.message.value;
 
   socket.emit('sendMessage', message);
+});
+
+// We send a 'sendLocation' event to server if the
+// geolocation api is available in their browser
+document.querySelector('#sendLocation').addEventListener('click', (e) => {
+  e.preventDefault();
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser');
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+    socket.emit('sendLocation', { latitude, longitude });
+  });
+});
+
+// Show gmap link with coords if we get 'location' event from server.
+socket.on('location', (coords) => {
+  console.log(
+    `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
+  );
 });
--- src/index.js
+++ src/index.js
@@ -15,13 +15,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New WebSocket connection');
 
+  // Send welcome message to joined user
+  // Send to everyone else that a user joined
   socket.emit('message', 'Welcome!');
   socket.broadcast.emit('message', 'A new user has joined!');
 
+  // Send message text to everyone when user send a message
   socket.on('sendMessage', (message) => {
     io.emit('message', message);
   });
 
+  // Send message to everyone when user share his location
+  socket.on('sendLocation', (coords) => {
+    io.emit('location', coords);
+  });
+
+  // Send message to everyone else when a user left
   socket.on('disconnect', () => {
     io.emit('message', 'A user has left!');
   });
